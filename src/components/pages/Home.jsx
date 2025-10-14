27c36bd1fbbca6548c803c97e079a0c2606eb244
Marwa Mohammed <marwamohammed@MacBook-Pro-949.local>
2025-10-13 20:38:55 -0500

    created content for home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,16 @@
-
+import {Link} from "react-router-dom";
+import pretty from "../../assets/home-image.jpeg";
+import"../../css/Home.css"
 
 function Home(){
-    return <div className="home-empty">
-        <p> This homepage is empty for now</p>
-    </div>
+    return (
+        <div className="home-container">
+            <img src={pretty} alt="home-image" className="home-image"/>
+            <Link to="/shop" className="home-shop-btn">
+                Shop Now
+            </Link>
+        </div>
+    );
 
 }
 
